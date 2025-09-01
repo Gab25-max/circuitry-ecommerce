0fa2cad8fa918000b7807335f8724ef142b7d2b5
leviatancode <gabbodlc03@gmail.com>
2025-09-01 13:19:48 +0200

    feat: add Google authentication flow with token handling and customer creation

--- circuitry-ecommerce-storefront/src/app/[countryCode]/(main)/account/@login/page.tsx
+++ circuitry-ecommerce-storefront/src/app/[countryCode]/(main)/account/@login/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next"
-
+import { sdk } from "@lib/sdk"
 import LoginTemplate from "@modules/account/templates/login-template"
 
 export const metadata: Metadata = {
@@ -7,6 +7,36 @@ export const metadata: Metadata = {
   description: "Sign in to your Medusa Store account.",
 }
 
+"use client" // include with Next.js 13+
+
+
+
 export default function Login() {
-  return <LoginTemplate />
+  const loginWithGoogle = async () => {
+    const result = await sdk.auth.login("customer", "google", {})
+
+    if (typeof result === "object" && result.location) {
+      // redirect to Google for authentication
+      window.location.href = result.location
+
+      return
+    }
+    
+    if (typeof result !== "string") {
+      // result failed, show an error
+      alert("Authentication failed")
+      return
+    }
+
+    // all subsequent requests are authenticated
+    const { customer } = await sdk.store.customer.retrieve()
+
+    console.log(customer)
+  }
+
+  return (
+    <div>
+      <button onClick={loginWithGoogle}>Login with Google</button>
+    </div>
+  )
 }
