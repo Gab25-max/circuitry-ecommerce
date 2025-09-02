bfeaaaca7654403f5f51d2ad8440952828654d59
leviatancode <gabbodlc03@gmail.com>
2025-09-02 12:14:06 +0200

    Implement code changes to enhance functionality and improve performance

--- circuitry-ecommerce-storefront/src/lib/sdk.ts
+++ circuitry-ecommerce-storefront/src/lib/sdk.ts
@@ -10,4 +10,8 @@ export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
   debug: process.env.NODE_ENV === "development",
   publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
+  auth: {
+    type: "session",                 // << importante
+    fetchCredentials: "include",     // assicura che i cookie di sessione viaggino
+  },
 })
